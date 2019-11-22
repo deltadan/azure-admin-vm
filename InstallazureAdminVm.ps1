b14597b7206c8d068c9c13739aa47b4604f6ffd4
Dan Patrick <dan@clouddeltav.com>
2019-11-22 15:25:20 -0500

    test commit

---
+++ InstallazureAdminVm.ps1
@@ -0,0 +1,47 @@
+#Enable Containers
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-All -All -NoRestart
+Enable-WindowsOptionalFeature -Online -FeatureName Containers -All -NoRestart
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart
+
+#Download Windows Subsystem for Linux - set the URL for the distro
+#Once VM restarts run bash.exe from the command
+$wslDownload = "https://aka.ms/wsl-ubuntu-1804"
+Invoke-WebRequest -Uri $wslDownload -OutFile Ubuntu.appx -UseBasicParsing
+Add-AppxPackage .\Ubuntu.appx
+
+#Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+#Assign Chocolatey Packages to Install
+$Packages = 'adobereader',`
+            'notepadplusplus.install',`
+            'winrar',`
+            'git',`
+            'putty.install',`
+            'sysinternals',`
+            'python3',`
+            'googlechrome',`
+            'docker-for-windows',`
+            'visualstudiocode',`
+            'git'
+
+#Install Chocolatey Packages
+ForEach ($PackageName in $Packages)
+{choco install $PackageName -y}
+
+#Assign Visual Studio Code Extensions to Install
+$Extentions = 'ms-azuretools.vscode-azureterraform',`
+              'ms-azuretools.vscode-docker',`
+              'ms-vscode.azure-account',`
+              'ms-vsts.team',`
+              'msazurermtools.azurerm-vscode-tools',`
+              'ms-vscode.azurecli',`
+              'ms-azure-devops.azure-pipelines',`
+              'ms-vscode.csharp'
+
+#Install Visual Studio Code Extensions
+ForEach ($ExtentionName in $Extentions)
+{& 'C:\Program Files\Microsoft VS Code\bin\code.cmd' --install-extension $ExtentionName}
+
+#Add Demo User to docker group
+#Add-LocalGroupMember -Member dan -Group docker-users
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# azure-admin-vm
-This vm is configured for use as an Azure Admin Station
+# Docker for Windows Dev Environment
+This one click deployment will build a Docker on Windows Development box in Azure.
+
+<a href="https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2Fdeltadan%2Fwindows10docker%2Fmaster%2Fazure-deploy.json" target="_blank">
+    <img src="http://azuredeploy.net/deploybutton.png"/>
+</a>
+
+**Software included on the VM**
+
+1. Docker for Windows Community Edition
+1. Visual Studio Community 2017 with Updates (.NET Desktop Environment, ASP and Web Development and Azure SDK)
+1. Git for Windows (bash)
+1. Visual Studio Code (Omnisharp CSharp)
+
+**Creds**
+1. User: stormtrooperio
+1. Password: Password.1!!
+
+**Starting Docker**
+1. Once the VM is deployed use the Azure portal to connect.
+1. Double click the Docker for Windows shortcut on the desktop.
+1. It will take a few minutes for Docker to start the first time.
+
+![alt text](https://github.com/deltadan/windows10docker/blob/master/media/dockerrun.png "Docker is Running on Windows 10 in Azure!")

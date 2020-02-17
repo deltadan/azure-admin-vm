d74491f7988ace6803191ad5c91c64ff83c0e80d
Dan Patrick <dan@clouddeltav.com>
2020-02-16 19:28:13 -0500

    remove the auto installer of wsl

--- InstallazureAdminVm.ps1
+++ InstallazureAdminVm.ps1
@@ -32,10 +32,3 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-WebRequest -Uri https:/
 Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu-1804 -OutFile ~/Ubuntu.appx -UseBasicParsing
 Add-AppxPackage -Path ~/Ubuntu.appx
 
-#Run Updates on Ubuntu
-RefreshEnv
-Ubuntu1804 install --root
-Ubuntu1804 run apt update
-
-#Reboot
-Restart-Computer -Force
